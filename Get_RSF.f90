53695fcebb8f2311d9ce7bccb8fac947d682ebeb
Andrew Friend <adfriend45@users.noreply.github.com>
2021-07-16 21:01:16 +0100

    Update Get_RSF.f90

--- Get_RSF.f90
+++ Get_RSF.f90
@@ -7,7 +7,7 @@ CHARACTER(LEN=200) :: file_name
 
 !WRITE (file_name, "(A15,I0.4,A4)") "RSF_plots", myrank, ".bin"
 !WRITE (file_name, "(A20,I0.4,A4)") "RUN2/SAVE5/RSF_plots", myrank, ".bin"
-WRITE (file_name, "(A0,I0.4,A4)") TRIM(RSF_In_file_name), myrank, ".bin"
+WRITE (file_name, "(A*,I0.4,A4)") TRIM(RSF_In_file_name), myrank, ".bin"
 write (*,*)file_name
 stop
 OPEN (10,FILE=file_name,FORM='UNFORMATTED')
