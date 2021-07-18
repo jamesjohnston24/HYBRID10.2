31198b98209021052479c289ca08ff4d004293df
Andrew Friend <adfriend45@users.noreply.github.com>
2021-07-18 13:50:47 +0100

    Just testing use on laptop.

---
+++ Get_RSF 2.f90
@@ -0,0 +1,18 @@
+SUBROUTINE Get_RSF
+
+USE mpi
+USE shared
+IMPLICIT NONE
+CHARACTER(LEN=200) :: file_name
+!test
+!WRITE (file_name, "(A15,I0.4,A4)") "RSF_plots", myrank, ".bin"
+!WRITE (file_name, "(A20,I0.4,A4)") "RUN2/SAVE5/RSF_plots", myrank, ".bin"
+WRITE (file_name, "(A,I0.4,A4)") TRIM(RSF_In_file_name), myrank, ".bin"
+OPEN (10,FILE=file_name,FORM='UNFORMATTED')
+READ (10) kyr_off
+READ (10) soilW_plot
+READ (10) B_plot
+READ (10) SOM_plot
+CLOSe (10)
+
+END SUBROUTINE Get_RSF
