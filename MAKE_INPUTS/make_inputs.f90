48ee6d6e9a923a853630f1d3ea4cc90fe709a011
Andrew Friend <adfriend45@users.noreply.github.com>
2021-07-29 14:22:16 +0100

    Update make_inputs.f90

--- MAKE_INPUTS/make_inputs.f90
+++ MAKE_INPUTS/make_inputs.f90
@@ -6,20 +6,36 @@ PROGRAM MAKE_INPUTS
 ! Create TRENDY input files for each processor.
 !----------------------------------------------------------------------!
 
+!----------------------------------------------------------------------!
 IMPLICIT NONE
+!----------------------------------------------------------------------!
 INTEGER :: kyr_clm
 CHARACTER(LEN=200) :: file_name, var_name
 CHARACTER(LEN=4) :: char_year
+!----------------------------------------------------------------------!
 
+!----------------------------------------------------------------------!
 kyr_clm = 2020
 var_name = 'tmp'
 WRITE (char_year, '(I4)') kyr_clm
 file_name = '/rds/user/adf10/rds-mb425-geogscratch/adf10/TRENDY2021/&
  &input/CRUJRA2021/'//'crujra.v2.2.5d.'//TRIM(var_name)//char_year//&
  &'.365d.noc.nc'
+CALL CHECK ( NF90_OPEN (TRIM (file_name), NF90_NOWRITE, ncid ))
+CALL CHECK ( NF90_CLOSE ( ncid ))
+!----------------------------------------------------------------------!
 
-!TRIM(var_name)//'/crujra.v2.1.5d.'&
-! &//TRIM(var_name)//'.'//char_year//'.365d.noc.nc'
+!----------------------------------------------------------------------!
+CONTAINS
+ SUBROUTINE check ( status )
+
+ INTEGER, INTENT ( in ) :: status
+ IF (status /= nf90_noerr) THEN
+  PRINT *, TRIM (NF90_STRERROR( status ))
+  STOP  "Stopped"
+ END IF
+ END SUBROUTINE check
+!----------------------------------------------------------------------!
 
 !----------------------------------------------------------------------!
 END PROGRAM MAKE_INPUTS
