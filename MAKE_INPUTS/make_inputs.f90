efcd14dbac7358f9246e81275238e66687f819f0
Andrew Friend <adfriend45@users.noreply.github.com>
2021-07-29 14:16:22 +0100

    Update make_inputs.f90

--- MAKE_INPUTS/make_inputs.f90
+++ MAKE_INPUTS/make_inputs.f90
@@ -12,7 +12,7 @@ CHARACTER(LEN=4) :: car_year
 
 var_name = 'tmp'
 file_name = '/rds/user/adf10/rds-mb425-geogscratch/adf10/TRENDY2021/&
- &input/CRUJRA2021/'//'crujra.v2.2.5d.'.//TRIM(var_name)//char_year//&
+ &input/CRUJRA2021/'//'crujra.v2.2.5d.'//TRIM(var_name)//char_year//&
  &'.365d.noc.nc'
 
 !TRIM(var_name)//'/crujra.v2.1.5d.'&
