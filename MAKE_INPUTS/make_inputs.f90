5b3980021c501b25d64db2c40f7e7e9fc716c786
Andrew Friend <adfriend45@users.noreply.github.com>
2021-07-29 14:01:14 +0100

    Update make_inputs.f90

--- MAKE_INPUTS/make_inputs.f90
+++ MAKE_INPUTS/make_inputs.f90
@@ -6,6 +6,18 @@ PROGRAM MAKE_INPUTS
 ! Create TRENDY input files for each processor.
 !----------------------------------------------------------------------!
 
+IMPLICIT NONE
+CHARACTER(LEN=200) :: file_name, var_name
+CHARACTER(LEN=4) :: car_year
+
+var_name = 'tmp'
+file_name = '/rds/user/adf10/rds-mb425-geogscratch/adf10/TRENDY2021/&
+ &input/CRUJRA2021/'//'crujra.v2.2.5d.'.//TRIM(var_name)//char_year//&
+ &'.365d.noc.nc'
+
+!TRIM(var_name)//'/crujra.v2.1.5d.'&
+! &//TRIM(var_name)//'.'//char_year//'.365d.noc.nc'
+
 !----------------------------------------------------------------------!
 END PROGRAM MAKE_INPUTS
 !======================================================================!
