48db8dab457c9f342eaeaab2eac1fe32654f2eff
Andrew Friend <adfriend45@users.noreply.github.com>
2021-07-30 17:58:26 +0100

    Update HYBRID10.3.f90

--- HYBRID10.3.f90
+++ HYBRID10.3.f90
@@ -12,7 +12,8 @@ USE mpi
 !----------------------------------------------------------------------!
 IMPLICIT NONE
 !----------------------------------------------------------------------!
-INTEGER :: error, nprocs, myrank
+INTEGER, PARAMETER :: ntimes = 1460, nland = 67420
+INTEGER :: error, nprocs, myrank, file_handle, buffer, size
 CHARACTER(LEN=200) :: file_name
 !----------------------------------------------------------------------!
 
@@ -28,9 +29,19 @@ CALL MPI_Comm_rank (MPI_COMM_WORLD,myrank,error)
 !----------------------------------------------------------------------!
 ! Read input data for this processor.
 !----------------------------------------------------------------------!
+size = ntimes * nland / nprocs
+ALLOCATE (buffer(ntime,nland/nprocs))
 WRITE (file_name, "(A,I0.4,A,I0.4,A)") "/home/adf10/rds/rds-mb425-geogscratch/&
 &adf10/TRENDY2021/input/CRUJRA2021/CRUJRA2021_",nprocs,&
 &"CPUs/",myrank,".bin"
+! Open the file for reading.
+CALL MPI_File_open(MPI_COMM_WORLD, file_name, &
+ MPI_MODE_RDONLY, MPI_INFO_NULL, file_handle, error)
+! MPI_IO is binary output format.
+CALL MPI_File_read(file_handle, buffer, size, &
+ MPI_REAL, MPI_STATUS_IGNORE, error)
+! Close the file.
+CALL MPI_File_Close(file_handle, error)
 !----------------------------------------------------------------------!
 
 !----------------------------------------------------------------------!
