a1148d3207530515c7e5281bdc72dff0ad915fc7
Andrew Friend <adfriend45@users.noreply.github.com>
2021-07-30 19:41:15 +0100

    Update PROCESS.f90

--- PROCESS.f90
+++ PROCESS.f90
@@ -38,6 +38,7 @@ CALL MPI_File_Close(file_handle, error)
 
 TB = SUM (B)
 WRITE (*,*) 'TB = ', TB
+write (*,*)B
 
 !----------------------------------------------------------------------!
 CALL MPI_FINALIZE ( error )
