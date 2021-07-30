a1471d2bf48b8836511fde368821d890bb50b026
Andrew Friend <adfriend45@users.noreply.github.com>
2021-07-30 19:17:48 +0100

    size

--- PROCESS.f90
+++ PROCESS.f90
@@ -1,2 +1,29 @@
 PROGRAM PROCESS
+IMPLICIT NONE
+
+INTEGER :: myrank, nprocs, size
+CHARACTER(LEN=200) :: var_name, file_name
+REAL, DIMENSION () :: B
+
+nprocs = 4
+size = nland / nprocs
+
+DO myrank = 1, nprocs
+
+ var_name = 'B'
+ WRITE (file_name, "(A,I0.4,A,A,I0.4,A,I0.4,A)") "/home/adf10/rds/rds-mb425-geogscratch/&
+ &adf10/TRENDY2021/output/HYBRID10.3_",nprocs,&
+ &"CPUs/",TRIM(var_name),kyr_clm,"_",myrank,".bin"
+ WRITE (*,*) 'Reading from ', TRIM(file_name)
+ ! Open the file for reading.
+ CALL MPI_File_open(MPI_COMM_WORLD, file_name, &
+  MPI_MODE_RDONLY, MPI_INFO_NULL, file_handle, error) 
+ ! MPI_IO is binary output format. Write using individual file pointer.
+ CALL MPI_File_read(file_handle, B, size, &
+  MPI_REAL, MPI_STATUS_IGNORE, error)
+ ! Close the file.
+ CALL MPI_File_Close(file_handle, error)
+
+END DO ! myrank = 1, nprocs
+
 END PROGRAM PROCESS
