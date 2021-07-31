441d2ae3a84e66cfa0ecb298e0e2ef99b25fc317
Andrew Friend <adfriend45@users.noreply.github.com>
2021-07-31 20:20:57 +0100

    Update PROCESS.f90

--- PROCESS.f90
+++ PROCESS.f90
@@ -7,7 +7,7 @@ IMPLICIT NONE
 
 INTEGER, PARAMETER :: nland = 67420
 INTEGER :: nprocs, error, myrank, nland_chunk, file_handle, kyr_clm
-REAL, ALLOCATABLE, DIMENSION (:) :: B_k
+REAL, ALLOCATABLE, DIMENSION (:) :: B_k, larea_k
 CHARACTER(LEN=20) :: var_name
 CHARACTER(LEN=200) :: file_name
 
@@ -23,10 +23,28 @@ CALL MPI_Comm_rank (MPI_COMM_WORLD,myrank,error)
 !----------------------------------------------------------------------!
 nland_chunk = nland / nprocs
 kyr_clm = 1901
+ALLOCATE (B_k(nland_chunk))
+ALLOCATE (larea_k(nland_chunk))
 !----------------------------------------------------------------------!
 
 !----------------------------------------------------------------------!
-ALLOCATE (B_k(nland_chunk))
+var_name = 'larea'
+ALLOCATE (larea_k (nland_chunk))
+WRITE (file_name, "(A,I0.4,A,A,A,I0.4,A)") "/home/adf10/rds/rds-mb425-geogscratch/&
+ &adf10/TRENDY2021/input/LUH2_GCB_2021/static_",nprocs,&
+ &"CPUs/",TRIM(var_name),"_",myrank,".bin"
+WRITE (*,*) 'Reading from ', TRIM(file_name)
+! Open the file for reading.
+CALL MPI_File_open(MPI_COMM_WORLD, file_name, &
+ MPI_MODE_RDONLY, MPI_INFO_NULL, file_handle, error) 
+! MPI_IO is binary output format. Write using individual file pointer.
+CALL MPI_File_read(file_handle, larea_k, nland_chunk, &
+ MPI_REAL, MPI_STATUS_IGNORE, error)
+! Close the file.
+CALL MPI_File_Close(file_handle, error)
+!----------------------------------------------------------------------!
+
+!----------------------------------------------------------------------!
 B_k = 20.0
 var_name = 'B'
 WRITE (file_name, "(A,I0.4,A,A,I0.4,A,I0.4,A)") "/home/adf10/rds/rds-mb425-geogscratch/&
@@ -43,7 +61,7 @@ CALL MPI_File_read(file_handle, B_k, nland_chunk, &
 CALL MPI_File_Close(file_handle, error)
 !----------------------------------------------------------------------!
 
-write (*,*) myrank, B_k (10), error
+write (*,*) myrank, B_k (10), larea_k (10)
 
 !----------------------------------------------------------------------!
 CALL MPI_FINALIZE ( error )
