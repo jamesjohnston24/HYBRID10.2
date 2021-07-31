6237a337351a6e4aadd32940f0f2ce0d91c0df0f
Andrew Friend <adfriend45@users.noreply.github.com>
2021-07-31 10:37:41 +0100

    Update PROCESS.f90

--- PROCESS.f90
+++ PROCESS.f90
@@ -6,7 +6,7 @@ INTEGER :: nland = 67420
 INTEGER :: myrank, nprocs, size, file_handle, kyr_clm, nland_chunk
 INTEGER :: error
 CHARACTER(LEN=200) :: var_name, file_name
-REAL :: TB
+REAL :: TB, TLA
 REAL, ALLOCATABLE, DIMENSION (:) :: B_k, larea_k
 
 !----------------------------------------------------------------------!
@@ -23,6 +23,22 @@ kyr_clm = 1921
 nland_chunk = nland / nprocs
 
 ALLOCATE (larea_k (nland_chunk))
+var_name = 'larea'
+ WRITE (file_name, "(A,I0.4,A,A,I0.4,A,I0.4,A)") "/home/adf10/rds/rds-mb425-geogscratch/&
+ &adf10/TRENDY2021/input/LUH2_GCB_2021/static_",nprocs,&
+ &"CPUs/",TRIM(var_name),kyr_clm,"_",myrank,".bin"
+WRITE (*,*) 'Reading from ', TRIM(file_name)
+! Open the file for reading.
+CALL MPI_File_open(MPI_COMM_WORLD, file_name, &
+ MPI_MODE_RDONLY, MPI_INFO_NULL, file_handle, error) 
+! MPI_IO is binary output format. Write using individual file pointer.
+CALL MPI_File_read(file_handle, larea_k, size, &
+ MPI_REAL, MPI_STATUS_IGNORE, error)
+! Close the file.
+CALL MPI_File_Close(file_handle, error)
+
+TLA = SUM (larea_k)
+WRITE (*,*) 'TLA = ',TLA
 
 ALLOCATE (B_k(nland_chunk))
 var_name = 'B'
