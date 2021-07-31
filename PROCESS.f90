4e6293d1ea65f059ea0b848f98da3aceb89dfa99
Andrew Friend <adfriend45@users.noreply.github.com>
2021-07-31 18:55:14 +0100

    Update PROCESS.f90

--- PROCESS.f90
+++ PROCESS.f90
@@ -5,24 +5,11 @@ use netCDF
 
 IMPLICIT NONE
 
-INTEGER, PARAMETER :: root = 0
-INTEGER, PARAMETER :: nland = 67420, nlon = 720, nlat = 360
-INTEGER :: myrank, nprocs, size, file_handle, kyr_clm, nland_chunk
-INTEGER :: error, i, j, k
-INTEGER :: lon_dimid, lat_dimid, lon_varid, lat_varid, ncid, varid
-INTEGER :: varidW, varidB, varidSOM
-INTEGER, DIMENSION (2) :: dimids_two
-CHARACTER(LEN=300) :: var_name, file_name
-CHARACTER(LEN=4) :: char_year
-REAL :: TB, TLA, summary
-REAL, PARAMETER :: fillvalue = 1.0E20
-REAL, ALLOCATABLE, DIMENSION (:) :: B_k, larea_k
-REAL, ALLOCATABLE, DIMENSION (:,:) :: soilW_grid
-REAL, ALLOCATABLE, DIMENSION (:,:) :: B_grid
-REAL, ALLOCATABLE, DIMENSION (:,:) :: SOM_grid
-INTEGER, ALLOCATABLE, DIMENSION (:) :: i_k, j_k
-REAL, DIMENSION (nlon) :: lon
-REAL, DIMENSION (nlat) :: lat
+INTEGER, PARAMETER :: nland = 67420
+INTEGER :: nprocs, error, myrank, nland_chunk, file_handle
+REAL, ALLOCATABLE, DIMENSION (:) :: B_k
+CHARACTER(LEN=20) :: var_name
+CHARACTER(LEN=200) :: file_name
 
 !----------------------------------------------------------------------!
 CALL MPI_INIT ( error )
@@ -34,7 +21,6 @@ CALL MPI_Comm_rank (MPI_COMM_WORLD,myrank,error)
 !----------------------------------------------------------------------!
 
 !----------------------------------------------------------------------!
-kyr_clm = 1901
 nland_chunk = nland / nprocs
 !----------------------------------------------------------------------!
 
@@ -50,7 +36,7 @@ WRITE (*,*) 'Reading from ', TRIM(file_name)
 CALL MPI_File_open(MPI_COMM_WORLD, file_name, &
  MPI_MODE_RDONLY, MPI_INFO_NULL, file_handle, error) 
 ! MPI_IO is binary output format. Write using individual file pointer.
-CALL MPI_File_read(file_handle, B_k, size, &
+CALL MPI_File_read(file_handle, B_k, nland_chunk, &
  MPI_REAL, MPI_STATUS_IGNORE, error)
 ! Close the file.
 CALL MPI_File_Close(file_handle, error)
