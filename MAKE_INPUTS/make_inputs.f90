288233d5a997d56eccb5a85ee7c24906b8d547af
Andrew Friend <adfriend45@users.noreply.github.com>
2021-07-31 16:53:41 +0100

    Update make_inputs.f90

--- MAKE_INPUTS/make_inputs.f90
+++ MAKE_INPUTS/make_inputs.f90
@@ -252,7 +252,7 @@ WRITE (*,*) 'Writing to ', TRIM(file_name)
 CALL MPI_File_open(MPI_COMM_WORLD, file_name, &
  MPI_MODE_WRONLY + MPI_MODE_CREATE, MPI_INFO_NULL, file_handle, error) 
 ! MPI_IO is binary output format. Write using individual file pointer.
-CALL MPI_File_write(file_handle, i, size/ntimes, &
+CALL MPI_File_write(file_handle, i_buffer, size/ntimes, &
  MPI_INTEGER, MPI_STATUS_IGNORE, error)
 ! Close the file.
 CALL MPI_File_Close(file_handle, error)
@@ -269,7 +269,7 @@ WRITE (*,*) 'Writing to ', TRIM(file_name)
 CALL MPI_File_open(MPI_COMM_WORLD, file_name, &
  MPI_MODE_WRONLY + MPI_MODE_CREATE, MPI_INFO_NULL, file_handle, error) 
 ! MPI_IO is binary output format. Write using individual file pointer.
-CALL MPI_File_write(file_handle, j, size/ntimes, &
+CALL MPI_File_write(file_handle, j_buffer, size/ntimes, &
  MPI_INTEGER, MPI_STATUS_IGNORE, error)
 ! Close the file.
 CALL MPI_File_Close(file_handle, error)
