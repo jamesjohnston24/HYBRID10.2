3cec876001f18cb1bb242680dd74a9ce687a2128
Andrew Friend <adfriend45@users.noreply.github.com>
2021-07-31 18:45:50 +0100

    Update PROCESS.f90

--- PROCESS.f90
+++ PROCESS.f90
@@ -33,64 +33,9 @@ CALL MPI_Comm_size (MPI_COMM_WORLD,nprocs,error)
 CALL MPI_Comm_rank (MPI_COMM_WORLD,myrank,error)
 !----------------------------------------------------------------------!
 
-IF (myrank == root) THEN
-
 !----------------------------------------------------------------------!
-!myrank = 1
 kyr_clm = 1901
-nprocs = 4
 nland_chunk = nland / nprocs
-!----------------------------------------------------------------------!
-
-!----------------------------------------------------------------------!
-var_name = 'larea'
-ALLOCATE (larea_k (nland_chunk))
-WRITE (file_name, "(A,I0.4,A,A,A,I0.4,A)") "/home/adf10/rds/rds-mb425-geogscratch/&
- &adf10/TRENDY2021/input/LUH2_GCB_2021/static_",nprocs,&
- &"CPUs/",TRIM(var_name),"_",myrank,".bin"
-WRITE (*,*) 'Reading from ', TRIM(file_name)
-! Open the file for reading.
-CALL MPI_File_open(MPI_COMM_WORLD, file_name, &
- MPI_MODE_RDONLY, MPI_INFO_NULL, file_handle, error) 
-! MPI_IO is binary output format. Write using individual file pointer.
-CALL MPI_File_read(file_handle, larea_k, nland_chunk, &
- MPI_REAL, MPI_STATUS_IGNORE, error)
-! Close the file.
-CALL MPI_File_Close(file_handle, error)
-!----------------------------------------------------------------------!
-
-!----------------------------------------------------------------------!
-var_name = 'i'
-ALLOCATE (i_k (nland_chunk))
-WRITE (file_name, "(A,I0.4,A,A,A,I0.4,A)") "/home/adf10/rds/rds-mb425-geogscratch/&
- &adf10/TRENDY2021/input/LUH2_GCB_2021/static_",nprocs,&
- &"CPUs/",TRIM(var_name),"_",myrank,".bin"
-WRITE (*,*) 'Reading from ', TRIM(file_name)
-! Open the file for reading.
-CALL MPI_File_open(MPI_COMM_WORLD, file_name, &
- MPI_MODE_RDONLY, MPI_INFO_NULL, file_handle, error) 
-! MPI_IO is binary output format. Write using individual file pointer.
-CALL MPI_File_read(file_handle, i_k, nland_chunk, &
- MPI_INTEGER, MPI_STATUS_IGNORE, error)
-! Close the file.
-CALL MPI_File_Close(file_handle, error)
-!----------------------------------------------------------------------!
-
-!----------------------------------------------------------------------!
-var_name = 'j'
-ALLOCATE (j_k (nland_chunk))
-WRITE (file_name, "(A,I0.4,A,A,A,I0.4,A)") "/home/adf10/rds/rds-mb425-geogscratch/&
- &adf10/TRENDY2021/input/LUH2_GCB_2021/static_",nprocs,&
- &"CPUs/",TRIM(var_name),"_",myrank,".bin"
-WRITE (*,*) 'Reading from ', TRIM(file_name)
-! Open the file for reading.
-CALL MPI_File_open(MPI_COMM_WORLD, file_name, &
- MPI_MODE_RDONLY, MPI_INFO_NULL, file_handle, error) 
-! MPI_IO is binary output format. Write using individual file pointer.
-CALL MPI_File_read(file_handle, j_k, nland_chunk, &
- MPI_INTEGER, MPI_STATUS_IGNORE, error)
-! Close the file.
-CALL MPI_File_Close(file_handle, error)
 !----------------------------------------------------------------------!
 
 !----------------------------------------------------------------------!
@@ -111,85 +56,7 @@ CALL MPI_File_Close(file_handle, error)
 !----------------------------------------------------------------------!
 
 !----------------------------------------------------------------------!
-ALLOCATE (soilW_grid(nlon,nlat))
-ALLOCATE (B_grid(nlon,nlat))
-ALLOCATE (SOM_grid(nlon,nlat))
-B_grid = fillvalue
-DO k = 1, nland_chunk
- i = i_k (k)
- j = j_k (k)
- B_grid (i,j) = B_k (k)
- write(*,*)i,j,k,B_grid(i,j)
-END DO ! k
-!----------------------------------------------------------------------!
-
-!----------------------------------------------------------------------!
-var_name = 'tmp'
-kyr_clm =1901
-WRITE (char_year, '(I4)') kyr_clm
-file_name = '/rds/user/adf10/rds-mb425-geogscratch/adf10/TRENDY2021/&
- &input/CRUJRA2021/'//'crujra.v2.2.5d.'//TRIM(var_name)//'.'//&
- &char_year//'.365d.noc.nc'
-WRITE (*,*) 'Opening file: ',file_name
-CALL CHECK ( NF90_OPEN (TRIM (file_name), NF90_NOWRITE, ncid ))
-varid = 2
-CALL CHECK ( NF90_GET_VAR ( ncid, varid, lon ))
-varid = 3
-CALL CHECK ( NF90_GET_VAR ( ncid, varid, lat ))
-CALL CHECK ( NF90_CLOSE ( ncid ))
-!----------------------------------------------------------------------!
-
-!----------------------------------------------------------------------!
-file_name = "fields_grid.nc"
-write (*, *) 'Writing to ', trim (file_name)
-CALL CHECK (NF90_CREATE (trim (file_name), cmode = nf90_clobber, &
-            ncid = ncid))
-CALL CHECK (NF90_DEF_DIM (ncid, "longitude", nlon, lon_dimid))
-CALL CHECK (NF90_DEF_DIM (ncid, "latitude" , nlat, lat_dimid))
-CALL CHECK (NF90_DEF_VAR (ncid, "longitude", nf90_float, lon_dimid, &
-            lon_varid))
-CALL CHECK (NF90_DEF_VAR (ncid, "latitude" , nf90_float, lat_dimid, &
-            lat_varid))
-dimids_two = (/ lon_dimid, lat_dimid /)
-CALL CHECK (NF90_PUT_ATT (ncid, lon_varid, "units", "degrees_east"))
-CALL CHECK (NF90_PUT_ATT (ncid, lat_varid, "units", "degrees_north"))
-CALL CHECK (NF90_DEF_VAR (ncid, "Soil_Water", nf90_float, &
-            dimids_two, varidW))
-CALL CHECK (NF90_DEF_VAR (ncid, "Living_Biomass", nf90_float, &
-            dimids_two, varidB))
-CALL CHECK (NF90_DEF_VAR (ncid, "Soil_Organic_Matter", nf90_float, &
-            dimids_two, varidSOM))
-CALL CHECK (NF90_PUT_ATT (ncid, varidW, "units", "m"))
-CALL CHECK (NF90_PUT_ATT (ncid, varidB, "units", "kg[DM] m-2"))
-CALL CHECK (NF90_PUT_ATT (ncid, varidSOM, "units", "kg[DM] m-2"))
-CALL CHECK (NF90_PUT_ATT (ncid, varidW, "_FillValue", fillvalue))
-CALL CHECK (NF90_PUT_ATT (ncid, varidB, "_FillValue", fillvalue))
-CALL CHECK (NF90_PUT_ATT (ncid, varidSOM, "_FillValue", fillvalue))
-CALL CHECK (NF90_ENDDEF (ncid))
-CALL CHECK (NF90_PUT_VAR (ncid, lon_varid, lon))
-CALL CHECK (NF90_PUT_VAR (ncid, lat_varid, lat))
-CALL CHECK (NF90_PUT_VAR (ncid,     varidW, soilW_grid))
-CALL CHECK (NF90_PUT_VAR (ncid,     varidB, B_grid))
-CALL CHECK (NF90_PUT_VAR (ncid,     varidSOM, SOM_grid))
-CALL CHECK (NF90_close (ncid))
-!----------------------------------------------------------------------!
-
-END IF ! myrank == root
-
-!----------------------------------------------------------------------!
 CALL MPI_FINALIZE ( error )
-!----------------------------------------------------------------------!
-
-!----------------------------------------------------------------------!
-CONTAINS
- SUBROUTINE check ( status )
-
- INTEGER, INTENT ( in ) :: status
- IF (status /= nf90_noerr) THEN
-  PRINT *, TRIM (NF90_STRERROR( status ))
-  STOP  "Stopped"
- END IF
- END SUBROUTINE check
 !----------------------------------------------------------------------!
 
 END PROGRAM PROCESS
