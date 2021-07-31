2a4ac921a36591a2457bd0304580f416470d89fb
Andrew Friend <adfriend45@users.noreply.github.com>
2021-07-31 14:07:16 +0100

    Update PROCESS.f90

--- PROCESS.f90
+++ PROCESS.f90
@@ -1,5 +1,7 @@
 PROGRAM PROCESS
 
+USE mpi
+
 IMPLICIT NONE
 
 INTEGER, PARAMETER :: root = 0
@@ -18,9 +20,15 @@ ALLOCATE (larea_k (nland_chunk))
 WRITE (file_name, "(A,I0.4,A,A,A,I0.4,A)") "/home/adf10/rds/rds-mb425-geogscratch/&
  &adf10/TRENDY2021/input/LUH2_GCB_2021/static_",nprocs,&
  &"CPUs/",TRIM(var_name),"_",myrank,".bin"
-OPEN (10,FILE=file_name,FORM='UNFORMATTED',STATUS='OLD')
-READ (10) larea_k
-CLOSE (10)
+WRITE (*,*) 'Reading from ', TRIM(file_name)
+! Open the file for reading.
+CALL MPI_File_open(MPI_COMM_WORLD, file_name, &
+ MPI_MODE_RDONLY, MPI_INFO_NULL, file_handle, error) 
+! MPI_IO is binary output format. Write using individual file pointer.
+CALL MPI_File_read(file_handle, larea_k, size, &
+ MPI_REAL, MPI_STATUS_IGNORE, error)
+! Close the file.
+CALL MPI_File_Close(file_handle, error)
 
 write (*,*) larea_k
 
