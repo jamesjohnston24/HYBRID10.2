cab5ed39137fce76c7d22769677d083dde78027c
Andrew Friend <adfriend45@users.noreply.github.com>
2021-07-31 16:34:14 +0100

    Update PROCESS.f90

--- PROCESS.f90
+++ PROCESS.f90
@@ -11,11 +11,13 @@ INTEGER :: error, k
 CHARACTER(LEN=200) :: var_name, file_name
 REAL :: TB, TLA, summary
 REAL, ALLOCATABLE, DIMENSION (:) :: B_k, larea_k
+INTEGER, ALLOCATABLE, DIMENSION (:) :: i_k, j_k
 
 !----------------------------------------------------------------------!
 CALL MPI_INIT ( error )
 !----------------------------------------------------------------------!
 
+!----------------------------------------------------------------------!
 var_name = 'larea'
 nprocs = 4
 myrank = 0
@@ -33,8 +35,49 @@ CALL MPI_File_read(file_handle, larea_k, nland_chunk, &
  MPI_REAL, MPI_STATUS_IGNORE, error)
 ! Close the file.
 CALL MPI_File_Close(file_handle, error)
+!----------------------------------------------------------------------!
 
-write (*,*) larea_k
+!----------------------------------------------------------------------!
+var_name = 'i'
+nprocs = 4
+myrank = 0
+nland_chunk = nland / nprocs
+ALLOCATE (i_k (nland_chunk))
+WRITE (file_name, "(A,I0.4,A,A,A,I0.4,A)") "/home/adf10/rds/rds-mb425-geogscratch/&
+ &adf10/TRENDY2021/input/LUH2_GCB_2021/static_",nprocs,&
+ &"CPUs/",TRIM(var_name),"_",myrank,".bin"
+WRITE (*,*) 'Reading from ', TRIM(file_name)
+! Open the file for reading.
+CALL MPI_File_open(MPI_COMM_WORLD, file_name, &
+ MPI_MODE_RDONLY, MPI_INFO_NULL, file_handle, error) 
+! MPI_IO is binary output format. Write using individual file pointer.
+CALL MPI_File_read(file_handle, i_k, nland_chunk, &
+ MPI_REAL, MPI_STATUS_IGNORE, error)
+! Close the file.
+CALL MPI_File_Close(file_handle, error)
+!----------------------------------------------------------------------!
+
+!----------------------------------------------------------------------!
+var_name = 'j'
+nprocs = 4
+myrank = 0
+nland_chunk = nland / nprocs
+ALLOCATE (j_k (nland_chunk))
+WRITE (file_name, "(A,I0.4,A,A,A,I0.4,A)") "/home/adf10/rds/rds-mb425-geogscratch/&
+ &adf10/TRENDY2021/input/LUH2_GCB_2021/static_",nprocs,&
+ &"CPUs/",TRIM(var_name),"_",myrank,".bin"
+WRITE (*,*) 'Reading from ', TRIM(file_name)
+! Open the file for reading.
+CALL MPI_File_open(MPI_COMM_WORLD, file_name, &
+ MPI_MODE_RDONLY, MPI_INFO_NULL, file_handle, error) 
+! MPI_IO is binary output format. Write using individual file pointer.
+CALL MPI_File_read(file_handle, j_k, nland_chunk, &
+ MPI_REAL, MPI_STATUS_IGNORE, error)
+! Close the file.
+CALL MPI_File_Close(file_handle, error)
+!----------------------------------------------------------------------!
+
+write (*,*) i_k
 
 !----------------------------------------------------------------------!
 CALL MPI_FINALIZE ( error )
