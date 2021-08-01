caff583a49f052e7bbdeeafa324f08310de6b3cd
Andrew Friend <adfriend45@users.noreply.github.com>
2021-08-01 19:37:08 +0100

    Update HYBRID10.3.f90

--- HYBRID10.3.f90
+++ HYBRID10.3.f90
@@ -15,10 +15,11 @@ IMPLICIT NONE
 INTEGER, PARAMETER :: ntimes = 1460, nland = 67420
 INTEGER :: t, k, nland_chunk
 INTEGER :: error, nprocs, myrank, file_handle, size, kyr_clm
-REAL :: dB, NPP, BL, fT, Tc
+REAL :: dB, NPP, BL, fT, Tc, ro
 REAL, PARAMETER :: dt = 21600.0
 REAL, PARAMETER :: tf = 273.15
-REAL, ALLOCATABLE, DIMENSION (:,:) :: tmp
+REAL, ALLOCATABLE, DIMENSION (:,:) :: tmp ! K
+REAL, ALLOCATABLE, DIMENSION (:,:) :: pre ! mm/6h
 REAL, ALLOCATABLE, DIMENSION (:) :: B
 CHARACTER(LEN=200) :: file_name, var_name
 !----------------------------------------------------------------------!
@@ -43,10 +44,27 @@ B = 0.0
 !----------------------------------------------------------------------!
 size = ntimes * nland / nprocs
 ALLOCATE (tmp(ntimes,nland/nprocs))
+ALLOCATE (pre(ntimes,nland/nprocs))
 DO kyr_clm = 1901, 1920
 
+ !---------------------------------------------------------------------!
  var_name = 'tmp'
- WRITE (file_name, "(A,I0.4,A,A,I0.4,A,I0.4,A)") "/home/adf10/rds/rds-mb425-geogscratch/&
+ WRITE (file_name, "(A,I0.4,A,A,I0.4,A,I0.4,A)") &
+ &"/home/adf10/rds/rds-mb425-geogscratch/&
+ &adf10/TRENDY2021/input/CRUJRA2021/CRUJRA2021_",nprocs,&
+ &"CPUs/",TRIM(var_name),kyr_clm,"_",myrank,".bin"
+ ! Open the file for reading.
+ CALL MPI_File_open(MPI_COMM_WORLD, file_name, &
+  MPI_MODE_RDONLY, MPI_INFO_NULL, file_handle, error)
+ ! MPI_IO is binary output format.
+ CALL MPI_File_read(file_handle, tmp, size, &
+  MPI_REAL, MPI_STATUS_IGNORE, error)
+ ! Close the file.
+ CALL MPI_File_Close(file_handle, error)
+ !---------------------------------------------------------------------!
+ var_name = 'pre'
+ WRITE (file_name, "(A,I0.4,A,A,I0.4,A,I0.4,A)") &
+ &"/home/adf10/rds/rds-mb425-geogscratch/&
  &adf10/TRENDY2021/input/CRUJRA2021/CRUJRA2021_",nprocs,&
  &"CPUs/",TRIM(var_name),kyr_clm,"_",myrank,".bin"
  ! Open the file for reading.
@@ -62,6 +80,7 @@ DO kyr_clm = 1901, 1920
  !---------------------------------------------------------------------!
  DO t = 1, ntimes
   DO k = 1, nland_chunk
+   ro = pre (t,k)
    Tc = tmp (t,k) - tf
    fT = 2.0 ** (0.1 * (Tc - 25.0)) / ((1.0 + EXP (0.3 * (Tc - 36.0))) * &
         (1.0 + EXP (0.3 * (0.0 - Tc))))
