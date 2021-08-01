9740d78cabc62f4d2f6c61e303eb95c56cac5240
Andrew Friend <adfriend45@users.noreply.github.com>
2021-08-01 18:40:15 +0100

    Update make_inputs.f90

--- MAKE_INPUTS/make_inputs.f90
+++ MAKE_INPUTS/make_inputs.f90
@@ -91,7 +91,7 @@ ALLOCATE (clm_buffer (ntimes,nland/nprocs))
 ALLOCATE (larea_buffer (nland/nprocs))
 ALLOCATE (i_buffer (nland/nprocs))
 ALLOCATE (j_buffer (nland/nprocs))
-DO kyr_clm = 1901, 1901
+DO kyr_clm = 1901, 1920
 
  var_name = 'pre'
 
