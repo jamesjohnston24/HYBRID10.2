b3153720af03b461e47c7f0b44eefd0296eedf0d
Andrew Friend <adfriend45@users.noreply.github.com>
2021-08-03 09:07:57 +0100

    Update PROCESS.f90

--- PROCESS.f90
+++ PROCESS.f90
@@ -40,7 +40,7 @@ CALL MPI_Comm_rank (MPI_COMM_WORLD,myrank,error)
 
 !----------------------------------------------------------------------!
 nland_chunk = nland / nprocs
-kyr_clm = 1901
+kyr_clm = 1902
 ALLOCATE (B_k        (nland_chunk))
 ALLOCATE (B_k_all    (nland))
 ALLOCATE (B_grid     (nlon,nlat))
