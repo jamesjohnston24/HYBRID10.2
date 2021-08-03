a7d5c215717089f4d5a8469a21e23af05b2d6047
Andrew Friend <adfriend45@users.noreply.github.com>
2021-08-03 18:17:55 +0100

    Update PROCESS.f90

--- PROCESS.f90
+++ PROCESS.f90
@@ -12,22 +12,27 @@ INTEGER, PARAMETER :: nland = 67420, nlon = 720, nlat = 360
 REAL, PARAMETER :: fillvalue = 1.0E20
 INTEGER :: nprocs, error, myrank, nland_chunk, file_handle, kyr_clm
 INTEGER :: i, j, k
-REAL :: TA, TB, TW, TSOM, TaNPP, Wmax, Bmax, SOMmax, aNPPmax
+REAL :: TA, TB, TW, TSOM, TaNPP, TaRh, RaNBP
+REAL :: Wmax, Bmax, SOMmax, aNPPmax, aRhmax, aNBPmax
 REAL, ALLOCATABLE, DIMENSION (:) :: B_k, larea_k, B_k_all, larea_k_all
 REAL, ALLOCATABLE, DIMENSION (:) :: soilW_k, soilW_k_all
 REAL, ALLOCATABLE, DIMENSION (:) :: SOM_k, SOM_k_all
 REAL, ALLOCATABLE, DIMENSION (:) :: aNPP_k, aNPP_k_all
+REAL, ALLOCATABLE, DIMENSION (:) :: aRh_k, aRh_k_all
+REAL, ALLOCATABLE, DIMENSION (:) :: aNBP_k, aNBP_k_all
 REAL, ALLOCATABLE, DIMENSION (:,:) :: soilW_grid
 REAL, ALLOCATABLE, DIMENSION (:,:) :: B_grid
 REAL, ALLOCATABLE, DIMENSION (:,:) :: SOM_grid
 REAL, ALLOCATABLE, DIMENSION (:,:) :: aNPP_grid
+REAL, ALLOCATABLE, DIMENSION (:,:) :: aRh_grid
+REAL, ALLOCATABLE, DIMENSION (:,:) :: aNBP_grid
 INTEGER, ALLOCATABLE, DIMENSION (:) :: i_k, j_k, i_k_all, j_k_all
 REAL, DIMENSION (nlon) :: lon
 REAL, DIMENSION (nlat) :: lat
 CHARACTER(LEN=20) :: var_name
 CHARACTER(LEN=200) :: file_name
 INTEGER :: lon_dimid, lat_dimid, lon_varid, lat_varid, ncid, varid
-INTEGER :: varidW, varidB, varidSOM, varidaNPP
+INTEGER :: varidW, varidB, varidSOM, varidaNPP, varidaRh, varidaNBP
 INTEGER, DIMENSION (2) :: dimids_two
 CHARACTER(LEN=4) :: char_year
 
@@ -55,6 +60,12 @@ ALLOCATE (SOM_grid (nlon,nlat))
 ALLOCATE (aNPP_k    (nland_chunk))
 ALLOCATE (aNPP_k_all(nland))
 ALLOCATE (aNPP_grid (nlon,nlat))
+ALLOCATE (aRh_k    (nland_chunk))
+ALLOCATE (aRh_k_all(nland))
+ALLOCATE (aRh_grid (nlon,nlat))
+ALLOCATE (aNBP_k    (nland_chunk))
+ALLOCATE (aNBP_k_all(nland))
+ALLOCATE (aNBP_grid (nlon,nlat))
 ALLOCATE (larea_k    (nland_chunk))
 ALLOCATE (larea_k_all(nland))
 ALLOCATE (i_k_all    (nland))
@@ -175,8 +186,40 @@ CALL MPI_File_read(file_handle, aNPP_k, nland_chunk, &
 CALL MPI_File_Close(file_handle, error)
 !----------------------------------------------------------------------!
 
+!----------------------------------------------------------------------!
+var_name = 'aRh'
+WRITE (file_name, "(A,I0.4,A,A,I0.4,A,I0.4,A)") "/home/adf10/rds/rds-mb425-geogscratch/&
+&adf10/TRENDY2021/output/HYBRID10.3_",nprocs,&
+&"CPUs/",TRIM(var_name),kyr_clm,"_",myrank,".bin"
+WRITE (*,*) 'Reading from ', TRIM(file_name)
+! Open the file for reading.
+CALL MPI_File_open(MPI_COMM_WORLD, file_name, &
+ MPI_MODE_RDONLY, MPI_INFO_NULL, file_handle, error) 
+! MPI_IO is binary output format. Write using individual file pointer.
+CALL MPI_File_read(file_handle, aRh_k, nland_chunk, &
+ MPI_REAL, MPI_STATUS_IGNORE, error)
+! Close the file.
+CALL MPI_File_Close(file_handle, error)
+!----------------------------------------------------------------------!
+
+!----------------------------------------------------------------------!
+var_name = 'aNBP'
+WRITE (file_name, "(A,I0.4,A,A,I0.4,A,I0.4,A)") "/home/adf10/rds/rds-mb425-geogscratch/&
+&adf10/TRENDY2021/output/HYBRID10.3_",nprocs,&
+&"CPUs/",TRIM(var_name),kyr_clm,"_",myrank,".bin"
+WRITE (*,*) 'Reading from ', TRIM(file_name)
+! Open the file for reading.
+CALL MPI_File_open(MPI_COMM_WORLD, file_name, &
+ MPI_MODE_RDONLY, MPI_INFO_NULL, file_handle, error) 
+! MPI_IO is binary output format. Write using individual file pointer.
+CALL MPI_File_read(file_handle, aNBP_k, nland_chunk, &
+ MPI_REAL, MPI_STATUS_IGNORE, error)
+! Close the file.
+CALL MPI_File_Close(file_handle, error)
+!----------------------------------------------------------------------!
+
 write (*,*) myrank, B_k (10), soilW_k (10), SOM_k (10), aNPP_k (10), &
- larea_k (10), i_k (10), j_k (10)
+ aRh_k (10, aNBP_k (10), larea_k (10), i_k (10), j_k (10)
 !B_grid = fillvalue
 !DO k = 1, nland_chunk
 ! i = i_k (k)
@@ -195,6 +238,10 @@ CALL MPI_Gather (SOM_k, nland_chunk, MPI_REAL, SOM_k_all, nland_chunk, MPI_REAL,
  root, MPI_COMM_WORLD, error)
 CALL MPI_Gather (aNPP_k, nland_chunk, MPI_REAL, aNPP_k_all, nland_chunk, MPI_REAL, &
  root, MPI_COMM_WORLD, error)
+CALL MPI_Gather (aRh_k, nland_chunk, MPI_REAL, aRh_k_all, nland_chunk, MPI_REAL, &
+ root, MPI_COMM_WORLD, error)
+CALL MPI_Gather (aNBP_k, nland_chunk, MPI_REAL, aNBP_k_all, nland_chunk, MPI_REAL, &
+ root, MPI_COMM_WORLD, error)
 CALL MPI_Gather (larea_k, nland_chunk, MPI_INTEGER, larea_k_all, nland_chunk, MPI_INTEGER, &
  root, MPI_COMM_WORLD, error)
 CALL MPI_Gather (i_k, nland_chunk, MPI_INTEGER, i_k_all, nland_chunk, MPI_INTEGER, &
@@ -209,14 +256,20 @@ TB = 0.0
 TW = 0.0
 TSOM = 0.0
 TaNPP = 0.0
+TaRh = 0.0
+RaNBP = 0.0
 Wmax = 0.0
 Bmax = 0.0
 SOMmax = 0.0
 aNPPmax = 0.0
+aRhmax = 0.0
+aNBPmax = 0.0
 B_grid = fillvalue
 soilW_grid = fillvalue
 SOM_grid = fillvalue
 aNPP_grid = fillvalue
+aRh_grid = fillvalue
+aNBP_grid = fillvalue
 DO k = 1, nland
  i = i_k_all (k)
  j = j_k_all (k)
@@ -224,15 +277,20 @@ DO k = 1, nland
  soilW_grid (i,j) = soilW_k_all (k)
  SOM_grid (i,j) = SOM_k_all (k)
  aNPP_grid (i,j) = aNPP_k_all (k)
+ aRh_grid (i,j) = aRh_k_all (k)
  TA = TA + larea_k_all (k)
  TB = TB + B_k_all (k) * larea_k_all (k)
  TW = TW + soilW_k_all (k) * larea_k_all (k)
  TSOM = TSOM + SOM_k_all (k) * larea_k_all (k)
  TaNPP = TaNPP + aNPP_k_all (k) * larea_k_all (k)
+ TaRh = TaRh + aRh_k_all (k) * larea_k_all (k)
+ TaNBP = TaNBP + aNBP_k_all (k) * larea_k_all (k)
  Wmax = MAX (soilW_k_all (k), Wmax)
  Bmax = MAX (B_k_all (k), Bmax)
  SOMmax = MAX (SOM_k_all (k), SOMmax)
  aNPPmax = MAX (aNPP_k_all (k), aNPPmax)
+ aRhmax = MAX (aRh_k_all (k), aNPPmax)
+ aNBPmax = MAX (aNBP_k_all (k), aNPPmax)
  !write(*,*)i,j,k,B_grid(i,j)
 END DO ! k
 WRITE (*,*) 'Total land area = ',TA
@@ -240,10 +298,14 @@ WRITE (*,*) 'Total biomass = ',TB/1.0E6
 WRITE (*,*) 'Total water = ',TW
 WRITE (*,*) 'Total SOM = ',TSOM/1.0e6
 WRITE (*,*) 'Total aNPP = ',TaNPP/1.0e6
+WRITE (*,*) 'Total aRh = ',TaRh/1.0e6
+WRITE (*,*) 'Total aNBP = ',TaNBP/1.0e6
 WRITE (*,*) 'Wmax = ', Wmax
 WRITE (*,*) 'Bmax = ', Bmax
 WRITE (*,*) 'SOMmax = ', SOMmax
 WRITE (*,*) 'aNPPmax = ', aNPPmax
+WRITE (*,*) 'aRhmax = ', aRhmax
+WRITE (*,*) 'aNBPmax = ', aNBPmax
 
 !----------------------------------------------------------------------!
 var_name = 'tmp'
@@ -283,14 +345,22 @@ CALL CHECK (NF90_DEF_VAR (ncid, "Soil_Organic_Matter", nf90_float, &
             dimids_two, varidSOM))
 CALL CHECK (NF90_DEF_VAR (ncid, "aNPP", nf90_float, &
             dimids_two, varidaNPP))
+CALL CHECK (NF90_DEF_VAR (ncid, "aRh", nf90_float, &
+            dimids_two, varidaRh))
+CALL CHECK (NF90_DEF_VAR (ncid, "aNBP", nf90_float, &
+            dimids_two, varidaNBP))
 CALL CHECK (NF90_PUT_ATT (ncid, varidW, "units", "m"))
 CALL CHECK (NF90_PUT_ATT (ncid, varidB, "units", "kg[DM] m-2"))
 CALL CHECK (NF90_PUT_ATT (ncid, varidSOM, "units", "kg[DM] m-2"))
 CALL CHECK (NF90_PUT_ATT (ncid, varidaNPP, "units", "kg[DM] m-2 yr-1"))
+CALL CHECK (NF90_PUT_ATT (ncid, varidaRh, "units", "kg[DM] m-2 yr-1"))
+CALL CHECK (NF90_PUT_ATT (ncid, varidaNBP, "units", "kg[DM] m-2 yr-1"))
 CALL CHECK (NF90_PUT_ATT (ncid, varidW, "_FillValue", fillvalue))
 CALL CHECK (NF90_PUT_ATT (ncid, varidB, "_FillValue", fillvalue))
 CALL CHECK (NF90_PUT_ATT (ncid, varidSOM, "_FillValue", fillvalue))
 CALL CHECK (NF90_PUT_ATT (ncid, varidaNPP, "_FillValue", fillvalue))
+CALL CHECK (NF90_PUT_ATT (ncid, varidaRh, "_FillValue", fillvalue))
+CALL CHECK (NF90_PUT_ATT (ncid, varidaNBP, "_FillValue", fillvalue))
 CALL CHECK (NF90_ENDDEF (ncid))
 CALL CHECK (NF90_PUT_VAR (ncid, lon_varid, lon))
 CALL CHECK (NF90_PUT_VAR (ncid, lat_varid, lat))
@@ -298,6 +368,8 @@ CALL CHECK (NF90_PUT_VAR (ncid,     varidW, soilW_grid))
 CALL CHECK (NF90_PUT_VAR (ncid,     varidB, B_grid))
 CALL CHECK (NF90_PUT_VAR (ncid,     varidSOM, SOM_grid))
 CALL CHECK (NF90_PUT_VAR (ncid,     varidaNPP, aNPP_grid))
+CALL CHECK (NF90_PUT_VAR (ncid,     varidaRh, aRh_grid))
+CALL CHECK (NF90_PUT_VAR (ncid,     varidaNBP, aNBP_grid))
 CALL CHECK (NF90_close (ncid))
 !----------------------------------------------------------------------!
 
