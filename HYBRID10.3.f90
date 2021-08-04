d7cea88a4f8d4be85be1436a59214ef51571d2b9
Andrew Friend <adfriend45@users.noreply.github.com>
2021-08-04 17:35:09 +0100

    Update HYBRID10.3.f90

--- HYBRID10.3.f90
+++ HYBRID10.3.f90
@@ -271,7 +271,6 @@ DO kyr_run = 1, nyr_run
  WRITE (*,*) 'Running kyr_run ', kyr_run, 'of', nyr_run,iyr
  DO t = 1, ntimes
   DO k = 1, nland_chunk
-tmp (t,k,iyr) = 298.0
    ro = soilW (k) + pre (t,k,iyr) / 1.0E3 - swc
    ro = MAX (0.0, ro)
    win = (pre (t,k,iyr) / 1.0e3 - ro) / dt
