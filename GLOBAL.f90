d20c1bcdd6a40bcb398aa13d8d515ca692b713fb
Andrew Friend <adfriend45@users.noreply.github.com>
2021-08-06 08:59:42 +0100

    Update GLOBAL.f90

--- GLOBAL.f90
+++ GLOBAL.f90
@@ -51,7 +51,7 @@ IMPLICIT NONE
   ! Read the data.
   call check( nf90_get_var(ncid, varid, data_in) )
 
-PRINT *, data_in (1,1:361)
+PRINT *, data_in (1,1:362)
 
   ! Check the data.
   do x = 1, NX
