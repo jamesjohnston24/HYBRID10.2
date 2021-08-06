eb46b761afd7780de1346319eab19e0328c9f187
Andrew Friend <adfriend45@users.noreply.github.com>
2021-08-06 16:14:45 +0100

    Update NBP.f90

--- NBP.f90
+++ NBP.f90
@@ -12,12 +12,24 @@ implicit none
 integer, parameter :: nyr = 2020 - 1901 + 1
 integer :: kyr, iyr
 real, dimension (nyr) :: t
+real :: B, SOM, fT
 
 open (10,file="tmp_mean.txt",status="old")
 do kyr = 1, nyr
  read (10,*) iyr, t (kyr)
 end do
 close (10)
+
+B = 0.0
+SOM = 0.0
+iyr = 1
+do kyr = 1, 100
+ iyr = iyr + 1
+ if (iyr > 20) kyr = 1
+ fT = 2.0 ** (0.1 * (t (iyr) - 25.0)) / ((1.0 + EXP (0.3 * (t (iyr) - 36.0))) * &
+      (1.0 + EXP (0.3 * (0.0 - t (iyr)))))
+ write (*,*) iyr,t(iyr),fT
+end do
 
 !----------------------------------------------------------------------!
 END PROGRAM NBP
