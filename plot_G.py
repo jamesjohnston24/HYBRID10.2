354766045fdf7573d537e3d93f4f9973eed57812
Andrew Friend <adfriend45@users.noreply.github.com>
2021-08-06 16:36:21 +0100

    Update plot_G.py

--- plot_G.py
+++ plot_G.py
@@ -1,13 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-y, t = np.loadtxt ('tmp_mean.txt', unpack=True)
+#y, t = np.loadtxt ('tmp_mean.txt', unpack=True)
+y, b, s, nbp = np.loadtxt ('transient.txt', unpack=True)
 
-s = 1961-1901+1-1
-e = 1990-1901+1-1
-base = sum (t[s:e]) / 30.0
-print (s, e, base)
+#s = 1961-1901+1-1
+#e = 1990-1901+1-1
+#base = sum (t[s:e]) / 30.0
+#print (s, e, base)
 
-plt.plot(y,t)
+#plt.plot(y,t)
+plt.plot (y, nbp)
 
 plt.show( )
