4418aa73ea35077084dd3414be9d0db33692414e
Andrew Friend <adfriend45@users.noreply.github.com>
2021-08-06 14:31:26 +0100

    Update plot_G.py

--- plot_G.py
+++ plot_G.py
@@ -3,6 +3,10 @@ import numpy as np
 
 y, t = np.loadtxt ('tmp_mean.txt', unpack=True)
 
-plt.plot(y,t)
+s = 1961-1901+1-1
+e = 1990-1901+1-1
+base = sum (t[s:e]) / 30.0
+
+plt.plot(y,t-base)
 
 plt.show( )
