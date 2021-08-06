8ee8dee4e6b2befeeb8607bf82986e9485ff4ab0
Andrew Friend <adfriend45@users.noreply.github.com>
2021-08-06 08:47:40 +0100

    Update GLOBAL.f90

--- GLOBAL.f90
+++ GLOBAL.f90
@@ -26,6 +26,56 @@ IMPLICIT NONE
 ! Use unidata.ucar.edu method (simple_xy_rd.f90)
 !----------------------------------------------------------------------!
 
+! This is the name of the data file we will read. 
+  character (len = *), parameter :: FILE_NAME = "/home/adf10/rds/&
+  &rds-mb425-geogscratch/adf10/TRENDY2021/input/LUH2_GCB_2021/&
+  &staticData_quarterdeg.nc"
+
+  ! We are reading 2D data, a 1440 x 720 grid. 
+  integer, parameter :: NX = 1440, NY = 720
+  integer :: data_in(NY, NX)
+
+  ! This will be the netCDF ID for the file and data variable.
+  integer :: ncid, varid
+
+  ! Loop indexes, and error handling.
+  integer :: x, y
+
+  ! Open the file. NF90_NOWRITE tells netCDF we want read-only access to
+  ! the file.
+  call check( nf90_open(FILE_NAME, NF90_NOWRITE, ncid) )
+
+  ! Get the varid of the data variable, based on its name.
+  call check( nf90_inq_varid(ncid, "vegetation_carbon_content", varid) )
+
+  ! Read the data.
+  call check( nf90_get_var(ncid, varid, data_in) )
+
+  ! Check the data.
+  do x = 1, NX
+     do y = 1, NY
+        if (data_in(y, x) /= (x - 1) * NY + (y - 1)) then
+           print *, "data_in(", y, ", ", x, ") = ", data_in(y, x)
+           stop "Stopped"
+        end if
+     end do
+  end do
+
+  ! Close the file, freeing all resources.
+  call check( nf90_close(ncid) )
+
+  print *,"*** SUCCESS reading example file ", FILE_NAME, "! "
+
+contains
+  subroutine check(status)
+    integer, intent ( in) :: status
+    
+    if(status /= nf90_noerr) then 
+      print *, trim(nf90_strerror(status))
+      stop "Stopped"
+    end if
+  end subroutine check
+
 !----------------------------------------------------------------------!
 
 !----------------------------------------------------------------------!
