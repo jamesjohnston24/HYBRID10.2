6350da890040c207ca4401dc685856cdb2de68ce
Andrew Friend <adfriend45@users.noreply.github.com>
2021-08-06 16:46:57 +0100

    Update plot_G.py

--- plot_G.py
+++ plot_G.py
@@ -4,6 +4,7 @@ import numpy as np
 #y, t = np.loadtxt ('tmp_mean.txt', unpack=True)
 y, b, s, nbp = np.loadtxt ('transient.txt', unpack=True)
 yr_inv, inv_flux = np.loadtxt ('inv_global.txt', delimiter=',', unpack=True)
+kyr10, hyr10, NPP10, Rh10, NEE10, B10, SOM10 = np.loadtxt ('/home/adf10/HYBRID10/RUN3/SAVE4/global_means06420.txt', unpack=True, skiprows=1)
 
 #s = 1961-1901+1-1
 #e = 1990-1901+1-1
@@ -15,5 +16,6 @@ b = 0.5
 y = y + 1900
 plt.plot (y, b*nbp)
 plt.plot(yr_inv, inv_flux)
+plt.plot(hyr10, a*NEE10,'y')
 
 plt.show( )
