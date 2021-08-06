587e0f74b53a19cbe9d265bfcf9bac1688342dcd
Andrew Friend <adfriend45@users.noreply.github.com>
2021-08-06 17:52:19 +0100

    Update NBP.f90

--- NBP.f90
+++ NBP.f90
@@ -11,9 +11,24 @@ PROGRAM NBP
 implicit none
 integer, parameter :: nyr = 2020 - 1901 + 1
 integer :: kyr, iyr
-real, dimension (nyr) :: t
-real :: B, SOM, fT, NPP, BL, dB, dSOM, ET_SOIL, EV, Rh
+real, dimension (nyr) :: t, CO2
+real :: B, SOM, fT, NPP, BL, dB, dSOM, ET_SOIL, EV, Rh, CO2
 real, parameter :: ga = 146376469.551773*1.0e9/1.0e15
+character(len=200) :: file_name
+
+file_name = '/home/adf10/rds/rds-mb425-geogscratch/adf10/TRENDY2021/&
+&input/CO2field/global_co2_ann_1700_2020.txt'
+open (10,file=file_name,status="old")
+iyr = 1
+do kyr = 1700, 1900
+ if (kyr<1901) then
+  read (10,*)
+ else
+  read (10,*)iyr,CO2(iyr)
+  iyr = iyr + 1
+ end if
+end do
+close (10)
 
 open (10,file="tmp_mean.txt",status="old")
 do kyr = 1, nyr
@@ -29,7 +44,7 @@ do kyr = 1, 3000
  if (iyr > 20) iyr = 1
  fT = 2.0 ** (0.1 * (t (iyr) - 25.0)) / ((1.0 + EXP (0.3 * (t (iyr) - 36.0))) * &
       (1.0 + EXP (0.3 * (0.0 - t (iyr)))))
- NPP = fT * 3.0 * 0.8
+ NPP = fT * 3.0 * 0.8 * CO2 (iyr) / CO2 (1)
  BL = B / 12.5
  dB = NPP - BL
  ET_SOIL = 0.0326 + 0.00351 * t (iyr) ** 1.652 - (0.023953 * t (iyr)) ** 7.19
@@ -47,7 +62,7 @@ open(20,file="transient.txt",status="unknown")
 do iyr = 1, nyr
  fT = 2.0 ** (0.1 * (t (iyr) - 25.0)) / ((1.0 + EXP (0.3 * (t (iyr) - 36.0))) * &
       (1.0 + EXP (0.3 * (0.0 - t (iyr)))))
- NPP = fT * 3.0 * 0.8
+ NPP = fT * 3.0 * 0.8 * CO2 (iyr) / CO2 (1)
  BL = B / 12.5
  dB = NPP - BL
  ET_SOIL = 0.0326 + 0.00351 * t (iyr) ** 1.652 - (0.023953 * t (iyr)) ** 7.19
